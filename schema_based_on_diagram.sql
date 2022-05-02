4f6ad2da4ca25a084417150f7f7e4a299c49f98d
Awais Amjed <awaisamjed171@gmail.com>
2022-05-02 18:19:40 +0500

    Add indexes on Foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -61,3 +61,10 @@ CREATE TABLE invoice_items (
         FOREIGN KEY (treatment_id)
             REFERENCES treatments(id)
 );
+
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoice_items (treatment_id);
+CREATE INDEX ON treatment_of_medical_histories (medical_history_id);
+CREATE INDEX ON treatment_of_medical_histories (treatment_id);
